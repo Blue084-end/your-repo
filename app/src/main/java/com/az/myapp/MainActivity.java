06b54aca987aa13bcb0e8f825f3c458e3bc57a7d
Blue084-end <azend11110000@gmail.com>
2025-09-09 11:43:12 +0700

    Create MainActivity.java

---
+++ app/src/main/java/com/az/myapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.az.myapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.example.com"); // bạn có thể đổi URL sau này
+        setContentView(webView);
+    }
+}
